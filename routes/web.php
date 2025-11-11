4073ffbbbbd3813c0987f84418fed2e502813a02
juanda0906 <juandavelasco0609@gmail.com>
2025-11-10 22:21:31 -0500

    commit2

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,6 @@ require __DIR__.'/auth.php';
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/eventos', function () {
+    return view('eventos');
+});
