b71a93c62f6729b531b8a56f537ab243d310875d
cris30000 <cristinasamsan2010@gmail.com>
2025-11-18 16:54:18 -0500

    mejorando index2

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::view('/adopta','adopta')->name('adopta');
 Route::view('/rescata','rescata')->name('rescata');
 Route::view('/contacto','contacto')->name('contacto');
 Route::view('/servicios','servicios')->name('servicios');
+//Route::view('/servicios','servicios')->name('servicios');
 
 
 // Rutas con autenticación
