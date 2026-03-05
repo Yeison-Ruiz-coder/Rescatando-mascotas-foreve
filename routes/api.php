8631823734a3c880d54a552b04f3811de2f6e503
Yeison-Ruiz-coder <Yeisonruiz7777777@gmail.com>
2026-03-05 00:02:02 -0500

    Conexion con postman

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\Api\TipoVacunaController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('tipos-vacunas', [TipoVacunaController::class, 'index']);
+Route::post('tipos-vacunas', [TipoVacunaController::class, 'store']);
+Route::put('tipos-vacunas/{id}', [TipoVacunaController::class, 'update']);
+Route::delete('tipos-vacunas/{id}', [TipoVacunaController::class, 'destroy']);
